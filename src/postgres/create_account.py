55b5b1f7a7362f6cd888c79be903a12537ae0f52
Jose Garcia <mendoza.joser51@gmail.com>
2022-11-12 21:23:33 -0700

    working query

--- src/postgres/create_account.py
+++ src/postgres/create_account.py
@@ -1,4 +1,5 @@
 import os
+
 from psycopg2 import connect, sql
 from dotenv import load_dotenv, find_dotenv
 from postgres.db_config import db_configuration
@@ -13,18 +14,12 @@ def create_new_account (user_info):
         connection = connect(**configuration)
         cur = connection.cursor()
 
-        columns = [key for key in user_info]
-        columns_string = ', '.join(columns)
-        values = list(user_info.values())
-        amount_val = ', '.join(['%s']) * len(values)
-        values_string = ','.join(values)
-        query = '''INSERT INTO %s (%s) VALUES(%s)'''%(os.getenv('POSTGRES_TABLE_NAME'), columns_string, amount_val)
-        cur.execute(query, values_string)
+        # columns = list(user_info.keys())
+        values = tuple(user_info.values())
+        
+        cur.execute(sql.SQL("INSERT INTO {} (first_name, middle_name, last_name, gender, email, date_of_birth, user_id, password) VALUES %s;").format(sql.Identifier(os.getenv('POSTGRES_TABLE_NAME'))),(values,))
+        connection.commit()
 
-        # postgres_command = f"INSERT INTO {os.getenv('POSTGRES_TABLE_NAME')} %s VALUES (%s)"
-        # print(f'postgres command: {postgres_command}')
-
-        # cur.execute(postgres_command,(columns_string,values_string))
     except(Exception) as error:
         print(error)
     finally:
